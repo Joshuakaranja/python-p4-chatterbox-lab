c993e7e96ad05d482ea03d3ef1f68043ed5b1fec
Joshua Karanja <jokaranja26@gmail.com>
2025-09-27 09:17:30 +0300

    stage files

--- server/app.py
+++ server/app.py
@@ -14,13 +14,48 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages',methods=['GET'])
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([{
+        "id":m.id,
+        "body":m.body,
+        "username":m.username,
+        "created_at":m.created_at.isoformat()
+    } for m in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    message = Message(body=data['body'], username=data['username'])
+    db.session.add(message)
+    db.session.commit()
+    return jsonify({
+        "id": message.id,
+        "body": message.body,
+        "username": message.username,
+        "created_at": message.created_at.isoformat()
+    })
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    message.body = data['body']
+    db.session.commit()
+    return jsonify({
+        "id": message.id,
+        "body": message.body,
+        "username": message.username,
+        "created_at": message.created_at.isoformat()
+    })
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({"message": "Message deleted"})
 
 if __name__ == '__main__':
     app.run(port=5555)
